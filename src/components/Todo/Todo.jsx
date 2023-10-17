19baf4241da1676d67b1bdbe5d51c27e89662639
Audrey RASOLONJATOVO <rasolonjatovo.audrey@gmail.com>
2023-10-17 14:28:03 +0200

    [CREATE] New component : TODO

---
+++ src/components/Todo/Todo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './todo.css'
+
+export default function Todo() {
+  return (
+    <article className="todo__container">
+      <h2 className="todo__title">blabla</h2>
+    </article>
+  )
+}
