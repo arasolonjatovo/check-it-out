b3d0d6c6886f7b2368bdd02d54b040d60b0d8d2e
Audrey RASOLONJATOVO <rasolonjatovo.audrey@gmail.com>
2023-10-17 14:33:09 +0200

    [ADD] New component to Todos' page

--- src/routes/Todos/Todos.jsx
+++ src/routes/Todos/Todos.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 
 import Button from '../../components/Button/Button'
+import Todo from '../../components/Todo/Todo'
 
 import './todos.css'
 
 export default function Todos() {
   return (
     <>
-      <header className='todos__container'>
-        <h1 className='todos__title'>MY TO-DOS</h1>
+      <header className="todos_header">
+        <h1 className="todos__title">MY TO-DOS</h1>
         <Button label="+ Add a todo" variant="primary" />
       </header>
+      <div id="todo__container">
+        <Todo />
+      </div>
     </>
   )
 }
