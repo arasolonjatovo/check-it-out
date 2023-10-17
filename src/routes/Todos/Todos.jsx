0396e7dbf8841f9adb8f7cd0bb33e838a5cb0414
Audrey RASOLONJATOVO <rasolonjatovo.audrey@gmail.com>
2023-10-17 13:47:38 +0200

    [ADD] Header's todos' page

--- src/routes/Todos/Todos.jsx
+++ src/routes/Todos/Todos.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
+import Button from '../../components/Button/Button'
+
+import './todos.css'
+
 export default function Todos() {
   return (
     <>
-      <h1>MY TO-DOS</h1>
+      <header className='todos__container'>
+        <h1 className='todos__title'>MY TO-DOS</h1>
+        <Button label="+ Add a todo" variant="primary" />
+      </header>
     </>
   )
 }
