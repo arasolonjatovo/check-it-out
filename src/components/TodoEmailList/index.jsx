146cb32ed5444ed096ab7a08d5985bb3b6798314
Lucas Miranda <mirandalucas52@gmail.com>
2023-10-18 14:24:33 +0200

    [FIX]svg attribute for console

--- src/components/TodoEmailList/index.jsx
+++ src/components/TodoEmailList/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import './TodoEmailList.css'
 import Button from '../Button/Button'
-import { db } from '../firebase/firebase'
+import { db } from '../../firebase/firebase'
 import { collection, getDocs, addDoc, deleteDoc, doc } from 'firebase/firestore'
 
 export default function TodoEmailList() {
@@ -68,10 +68,10 @@ export default function TodoEmailList() {
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="lucide lucide-x"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-x"
               >
                 <path d="M18 6 6 18" />
                 <path d="m6 6 12 12" />
