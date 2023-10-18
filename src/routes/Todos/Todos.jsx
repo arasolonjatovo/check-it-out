0fd07eec8dc398338442c37bf098c9f04c0b25ba
Drea <102946173+DreaDrea8@users.noreply.github.com>
2023-10-18 15:29:11 +0200

    Update Todos.jsx

--- src/routes/Todos/Todos.jsx
+++ src/routes/Todos/Todos.jsx
@@ -5,12 +5,36 @@ import Todo from '../../components/Todo/Todo'
 
 import './todos.css'
 
+import { addDoc, doc, setDoc, collection } from 'firebase/firestore'
+import { db, auth } from '../../firebase/firebase'
+import { v4 as uuidv4 } from 'uuid'
+
 export default function Todos() {
+  const handleAddTodo = async () => {
+    const TodoId = uuidv4() // Générez un nouvel ID pour chaque tâche
+
+    const todoData = {
+      // Autres données de tâche
+    }
+
+    try {
+      // Utilisez doc() pour spécifier le nom du document comme l'ID de la tâche
+      await setDoc(doc(db, 'Todos', TodoId), todoData)
+      console.log('Tâche ajoutée avec succès')
+    } catch (error) {
+      console.log("Erreur lors de l'ajout de la tâche")
+    }
+  }
+
   return (
     <>
       <header className="todos_header">
-        <h1 className="todos__title">MY TO-DOS</h1>
-        <Button label="+ Add a todo" variant="primary" />
+        <h1 className="todos__title">MES TÂCHES À FAIRE</h1>
+        <Button
+          handleClick={handleAddTodo}
+          label="+ Ajouter une tâche"
+          variant="primary"
+        />
       </header>
       <div id="todo__container">
         <Todo />
