0b5f4df86a2e52c5161baed55bafcae25071ec87
Lucas Miranda <mirandalucas52@gmail.com>
2023-10-18 21:05:21 +0200

    [FIX] to test firebase

--- src/components/TodoEmailList/index.jsx
+++ src/components/TodoEmailList/index.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import './TodoEmailList.css'
 import Button from '../Button/Button'
 import { db } from '../../firebase/firebase'
-import { collection, getDocs, addDoc, deleteDoc, doc } from 'firebase/firestore'
+import { collection, getDocs, addDoc } from 'firebase/firestore'
 
 export default function TodoEmailList() {
   const [newEmail, setNewEmail] = useState('')
@@ -39,45 +39,13 @@ export default function TodoEmailList() {
       console.log('Nouveau document ID :', docRef.id)
     }
   }
-  const deleteEmail = async (id) => {
-    try {
-      await deleteDoc(doc(db, 'test', id))
-      // Met à jour l'état local pour exclure l'e-mail supprimé
-      const updatedTodos = todos.filter((todo) => todo.id !== id)
-      setTodos(updatedTodos)
-    } catch (error) {
-      console.error("Erreur lors de la suppression de l'e-mail :", error)
-    }
-  }
 
   return (
     <div className="emailListContainer">
       <h2>Access to my to-do:</h2>
       <ul className="emailList">
         {todos.map((todo, index) => (
-          <li key={index}>
-            {todo.email}{' '}
-            <button
-              className="deleteEmail"
-              onClick={() => deleteEmail(todo.id)}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="lucide lucide-x"
-              >
-                <path d="M18 6 6 18" />
-                <path d="m6 6 12 12" />
-              </svg>
-            </button>
-          </li>
+          <li key={index}>{todo.email}</li>
         ))}
       </ul>
       <input
